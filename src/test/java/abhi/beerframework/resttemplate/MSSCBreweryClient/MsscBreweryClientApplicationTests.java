ed1b88638d68eb480ddfe76df320c3259e88afc7
Abhi0492 <abhi.iyer.abhishek@gmail.com>
2021-07-13 15:22:58 +0530

    MSSC Brewery Client using REST template commit 1

---
+++ src/test/java/abhi/beerframework/resttemplate/MSSCBreweryClient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package abhi.beerframework.resttemplate.MSSCBreweryClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
